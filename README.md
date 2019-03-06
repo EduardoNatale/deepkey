58eeee5d8a54c0cd0385f7d17784f6139ae45821
EduardoNatale <notebook@pop-os.localdomain>
2019-03-06 19:08:23 -0300

    Adding the layout

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# deepkey
- A project to detect if the user is itself or not using just the keyboard.
+# DeepKey
+A project to detect if the user is itself or not using just the keyboard.
+
+Project using Django, JavaScript and Python.
--- project/deepkey/static/assets/javascript/deepkey.js
+++ project/deepkey/static/assets/javascript/deepkey.js
@@ -1,8 +1,25 @@
 const input = document.getElementById('input-password')
 const input1 = document.getElementById('input-password1')
+const tableBody = document.querySelector('.table');
+let id = 0
+
+function add2table(e){
+  let row = document.createElement('tr');
+  row.innerHTML += '<td>' + id + '</td>';
+  row.innerHTML += '<td>' + e.key + '</td>';
+  row.innerHTML += '<td>' + e.keyCode + '</td>';
+  tableBody.appendChild(row);
+  id += 1;
+}
+
+function ScrollDiv(){
+  if(document.getElementById('ecran').scrollTop<(document.getElementById('ecran').scrollHeight-document.getElementById('ecran').offsetHeight)){-1
+    document.getElementById('ecran').scrollTop=document.getElementById('ecran').scrollTop+1000;
+  }
+}
 
 input.onkeydown = function(e) {
-
     input1.value = e.key;
-
+    add2table(e);
+    ScrollDiv();
 }
