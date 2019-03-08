fbac9738eea2f1dea1b705557d0240ec705c4530
EduardoNatale <notebook@pop-os.localdomain>
2019-03-08 17:03:55 -0300

    .

--- project/deepkey/static/assets/javascript/deepkey.js
+++ project/deepkey/static/assets/javascript/deepkey.js
@@ -14,35 +14,35 @@ let lTable3 = []
 
 function add2table1(){
   second += 1;
+  lTable1.push([id1, manyTimes, second]);
   let row = document.createElement('tr');
   row.innerHTML += '<td>' + id1 + '</td>';
   row.innerHTML += '<td>' + manyTimes + '</td>';
   row.innerHTML += '<td>' + second + '</td>';
   tableBody1.appendChild(row);
-  lTable1.push([id1, manyTimes, second]);
   id1 += 1;
   manyTimes = 0;
 }
 
 function add2table2(e){
+  lTable2.push([id2, e.key, e.keyCode]);
   let row = document.createElement('tr');
   row.innerHTML += '<td>' + id2 + '</td>';
   row.innerHTML += '<td>' + e.key + '</td>';
   row.innerHTML += '<td>' + e.keyCode + '</td>';
   tableBody2.appendChild(row);
-  lTable2.push([id2, e.key, e.keyCode]);
   id2 += 1;
 }
 
 function add2table3(type) {
   let d = new Date();
   let n = d.getTime();
+  lTable3.push([id3, n, type]);
   let row = document.createElement('tr');
   row.innerHTML += '<td>' + id3 + '</td>';
   row.innerHTML += '<td>' + n + '</td>';
   row.innerHTML += '<td>' + type + '</td>';
   tableBody3.appendChild(row);
-  lTable3.push([id3, n, type]);
   id3 += 1;
 }
 
--- project/deepkey/views.py
+++ project/deepkey/views.py
@@ -3,21 +3,20 @@ import csv
 
 def deepkey(request):
     if request.method == 'POST':
-        table1 = brokenData(request.POST.get('inputTable1', None), 3)
-        table2 = brokenData(request.POST.get('inputTable2', None), 3)
-        table3 = brokenData(request.POST.get('inputTable3', None), 3)
+        if request.POST.get('input-password', None) == 'qwerty123':
+            table1 = brokenData(request.POST.get('inputTable1', None), 3)
+            table2 = brokenData(request.POST.get('inputTable2', None), 3)
+            table3 = brokenData(request.POST.get('inputTable3', None), 3)
 
-
-
-        with open('tabela1.csv', 'a') as csv1:
-            writer = csv.writer(csv1)
-            writer.writerows(table1)
-        with open('tabela2.csv', 'a') as csv2:
-            writer = csv.writer(csv2)
-            writer.writerows(table2)
-        with open('tabela3.csv', 'a') as csv3:
-            writer = csv.writer(csv3)
-            writer.writerows(table3)
+            with open('tabela1.csv', 'a') as csv1:
+                writer = csv.writer(csv1)
+                writer.writerows(table1)
+            with open('tabela2.csv', 'a') as csv2:
+                writer = csv.writer(csv2)
+                writer.writerows(table2)
+            with open('tabela3.csv', 'a') as csv3:
+                writer = csv.writer(csv3)
+                writer.writerows(table3)
 
         return render(request, 'index.html')
     return render(request, 'index.html')
@@ -27,9 +26,9 @@ def brokenData(data, n):
     listona = []
     data = data.split(',')
     for i in data:
+        listinha.append(i)
         if len(listinha) == n:
             listona.append(listinha)
             listinha = []
-        listinha.append(i)
 
     return listona
