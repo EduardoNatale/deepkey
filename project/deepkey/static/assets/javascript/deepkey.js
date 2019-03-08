72e9d61efba3f372a971d5d581bed7dfda92004e
EduardoNatale <notebook@pop-os.localdomain>
2019-03-08 15:30:34 -0300

    CSV

--- project/deepkey/static/assets/javascript/deepkey.js
+++ project/deepkey/static/assets/javascript/deepkey.js
@@ -8,14 +8,18 @@ let id3 = 0;
 let manyTimes = 0;
 let second = 0;
 
+let lTable1 = []
+let lTable2 = []
+let lTable3 = []
+
 function add2table1(){
-  // alert('dsa');
   second += 1;
   let row = document.createElement('tr');
   row.innerHTML += '<td>' + id1 + '</td>';
   row.innerHTML += '<td>' + manyTimes + '</td>';
   row.innerHTML += '<td>' + second + '</td>';
   tableBody1.appendChild(row);
+  lTable1.push([id1, manyTimes, second]);
   id1 += 1;
   manyTimes = 0;
 }
@@ -26,7 +30,20 @@ function add2table2(e){
   row.innerHTML += '<td>' + e.key + '</td>';
   row.innerHTML += '<td>' + e.keyCode + '</td>';
   tableBody2.appendChild(row);
+  lTable2.push([id2, e.key, e.keyCode]);
   id2 += 1;
+}
+
+function add2table3(type) {
+  let d = new Date();
+  let n = d.getTime();
+  let row = document.createElement('tr');
+  row.innerHTML += '<td>' + id3 + '</td>';
+  row.innerHTML += '<td>' + n + '</td>';
+  row.innerHTML += '<td>' + type + '</td>';
+  tableBody3.appendChild(row);
+  lTable3.push([id3, n, type]);
+  id3 += 1;
 }
 
 function scrollDiv1(){
@@ -51,7 +68,35 @@ input.onkeydown = function(e) {
     add2table2(e);
     scrollDiv2();
     manyTimes += 1;
+    add2table3('DOWN');
+    scrollDiv3();
 }
+
+input.onkeyup = function(e) {
+    add2table3('UP');
+    scrollDiv3();
+}
+
+function myFunction() {
+  document.getElementById('inputTable1').value = lTable1;
+  document.getElementById('inputTable2').value = lTable2;
+  document.getElementById('inputTable3').value = lTable3;
+}
+
+// $("#button").click(function(e) {
+//   e.preventDefault();
+//   $.ajax({
+//     type: "POST",
+//     url: 'http://127.0.0.1:8000/',
+//     data: {
+//         'lTable1': lTable1,
+//         'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
+//     },
+//     sucess: function() {
+//       alert('da');
+//     }
+//   });
+// });
 
 setInterval(function(){
   add2table1();
--- project/deepkey/views.py
+++ project/deepkey/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+import csv
 
 def deepkey(request):
     if request.method == 'POST':
-        pass
+        table1 = brokenData(request.POST.get('inputTable1', None), 3)
+        table2 = brokenData(request.POST.get('inputTable2', None), 3)
+        table3 = brokenData(request.POST.get('inputTable3', None), 3)
+        with open('tabela1.csv', 'a') as csv1:
+            writer = csv.writer(csv1)
+            writer.writerows(table1)
+        with open('tabela2.csv', 'a') as csv2:
+            writer = csv.writer(csv2)
+            writer.writerows(table2)
+        with open('tabela3.csv', 'a') as csv3:
+            writer = csv.writer(csv3)
+            writer.writerows(table3)
+
+        return render(request, 'index.html')
     return render(request, 'index.html')
+
+def brokenData(data, n):
+    listinha = []
+    listona = []
+    data = data.split(',')
+    for i in data:
+        if len(listinha) == n:
+            listona.append(listinha)
+            listinha = []
+        listinha.append(i)
+
+    return listona
